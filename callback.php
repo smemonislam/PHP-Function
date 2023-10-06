d4bbc689fe5b85183f40c0a68fc0d0256b3ecd65
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-10-06 20:36:14 +0600

    Callback Problem 06 Soleved

--- callback.php
+++ callback.php
@@ -7,8 +7,7 @@
  */
 
 $numbers = [-1, 2, 0, 5, -3, 10];
-
-
+// Solution 01 with coustom callback function
 function coustomFilter($arr, $cb)
 {
     $newArray = [];
@@ -24,104 +23,105 @@ function coustomFilter($arr, $cb)
 $positive = coustomFilter($numbers, function ($value) {
     return $value > 0;
 });
-print_r($positive);
+// print_r($positive);
 
-// function filterPositive($arr)
-// {
-//     echo $arr . "\n";
-//     //return $arr > 0;
-// }
-
-// $positiveNumber = array_filter($numbers, function ($numbersValue) {
-//     return $numbersValue > 0;
-// });
+// Solution 02 built-in functions in php
+$positiveNumber = array_filter($numbers, function ($numbersValue) {
+    return $numbersValue > 0;
+});
 // print_r($positiveNumber);
 
 
-// /**
-//  * 02.
-//  * Filter Names by Length: Create a callback function that filters an array of names based on their length
-//  */
+/**
+ * 02.
+ * Filter Names by Length: Create a callback function that filters an array of names based on their length
+ */
 
-// $names = ["John", "Alice", "Bob", "Eve", "Anna"];;
+$names = ["John", "Alice", "Bob", "Eve", "Anna"];;
 
 
 
-// function filterLength($arr, $length)
-// {
-//     return array_filter($arr, function ($name) use ($length) {
-//         $wordLength = mb_strlen($name);
-//         return $wordLength == $length;
-//     });
-// }
+function filterLength($arr, $length)
+{
+    return array_filter($arr, function ($name) use ($length) {
+        $wordLength = mb_strlen($name);
+        return $wordLength == $length;
+    });
+}
 
-// $wordLength = filterLength($names, 4);
+$wordLength = filterLength($names, 4);
 // print_r($wordLength);
 
 
-// /**
-//  * 03.
-//  * Calculate Total: Create a callback function that calculates the total sum of values in an array.
-//  */
+/**
+ * 03.
+ * Calculate Total: Create a callback function that calculates the total sum of values in an array.
+ */
 
-// $values = [2, 5, 8, 10];
+$values = [2, 5, 8, 10];
 
-// $sum = array_reduce($values, function ($a, $b) {
-//     return $a + $b;
-// }, 0);
+$sum = array_reduce($values, function ($a, $b) {
+    return $a + $b;
+}, 0);
 
 // echo $sum;
 
 
-// /**
-//  * 04.
-//  * Convert to Uppercase: Create a callback function that converts each element of an array to uppercase. 
-//  */
+/**
+ * 04.
+ * Convert to Uppercase: Create a callback function that converts each element of an array to uppercase. 
+ */
 
-// $words = ["apple", "banana", "cherry"];
+$words = ["apple", "banana", "cherry"];
 
-// $uppercase = array_map(function ($word) {
-//     return strtoupper($word);
-// }, $words);
+$uppercase = array_map(function ($word) {
+    return strtoupper($word);
+}, $words);
 
 // print_r($uppercase);
 
 
-// /**
-//  * 05.
-//  * Custom Sorting: Create a callback function that sorts an array of strings based on a custom sorting criteria 
-//  * e.g., string length).
-//  */
-// $strings = ["apple", "banana", "cherry", "date", "fig"];
+/**
+ * 05.
+ * Custom Sorting: Create a callback function that sorts an array of strings based on a custom sorting criteria 
+ * e.g., string length).
+ */
+$strings = ["apple", "banana", "cherry", "date", "fig"];
 
-// function sortByAuthorName($arr1, $arr2)
-// {
-//     if (strlen($arr1) > strlen($arr2)) {
-//         return -1;
-//     } elseif (strlen($arr1) < strlen($arr2)) {
-//         return 1;
-//     } else {
-//         return 0;
-//     }
-// }
+function sortByAuthorName($arr1, $arr2)
+{
+    if (strlen($arr1) > strlen($arr2)) {
+        return -1;
+    } elseif (strlen($arr1) < strlen($arr2)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
-// usort($strings, "sortByAuthorName");
+usort($strings, "sortByAuthorName");
 // print_r($strings);
 
 
+/**
+ * 06.
+ * Array Mapping: Create a callback function that maps each element of an array to a new value (e.g., 
+ * square each number in the array). 
+ */
 
-// function abc()
-// {
-//     echo 5 + 5;
-// }
+$square = [2, 5, 8, 10];
+function customMap($arr, $cb)
+{
+    $newArray = [];
+    foreach ($arr as $value) {
+        $newValue = $cb($value);
+        array_push($newArray, $newValue);
+    }
+    return $newArray;
+}
 
+$sq = customMap($square, function ($value) {
+    return $value * $value;
+});
 
-// $abcd = function () {
-//     echo 5 + 5;
-// };
-// function CallBack($param2)
-// {
-//     $param2();
-// }
-
-// CallBack('abc');
+print_r($sq);
